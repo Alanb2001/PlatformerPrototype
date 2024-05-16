7d51df2c95f033c5e9ab4a374bb83c3797125be3
Alanb2001 <alanb2002@gmail.com>
2024-05-16 21:42:17 +0100

    first pass of a game manger, at the moment it checks how many coins there are in the level and the player needs to collect all of them to move onto the next level

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int coinsInLevel;
+    private int coinsCollected;
+
+    private void OnEnable()
+    {
+        Coin.OnCoinCollected += CoinCount;
+    }
+
+    private void OnDisable()
+    {
+        Coin.OnCoinCollected -= CoinCount;
+    }
+
+    private void Awake()
+    {
+        coinsInLevel = FindObjectsOfType<Coin>().Length;
+
+        print(coinsInLevel);
+
+        DontDestroyOnLoad(this);
+    }
+    
+    IEnumerator LevelChange()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(1); 
+    }
+
+    private void CoinCount()
+    {
+        coinsCollected++;
+        
+        if (coinsCollected == coinsInLevel)
+        {
+            StartCoroutine(LevelChange());
+            print("Next level");
+        }
+    }
+}
